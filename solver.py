38fbd9342d5bc32983249e989530c52c5c10275b
zelano47 <andrew_zelano@my.uri.edu>
2020-03-04 17:17:38 -0500

    working

--- solver.py
+++ solver.py
@@ -3,6 +3,7 @@ import rubik
 def construct_path(overlap, Ldict, Rdict):
     # prepend moves to left list
     # append moves to right list
+
     Lpath = []
     Rpath = []
     Lcurr = overlap
@@ -15,9 +16,9 @@ def construct_path(overlap, Ldict, Rdict):
     while True:
         if Rdict[Rcurr][0] == 'e':
             break
-        Rpath.append(Rdict[Rcurr][0])
+        Rpath.append(rubik.perm_inverse(Rdict[Rcurr][0]))
         Rcurr = Rdict[Rcurr][1]
-    
+
     return Lpath + Rpath
 
 
@@ -42,14 +43,13 @@ def shortest_path(start, end):
     depth = 0
     while True:
         if depth > 7:
-            print("Error: Invalid configuration")
             return None
         newLCount = 0
         for i in range(Lcount):
             curr = LQueue.pop(0)
             if curr in Rdict:
                 overlap = curr
-                return construct_path(overlap,Ldict, Rdict)
+                return construct_path(overlap, Ldict, Rdict)
             else:
                 for move in rubik.quarter_twists:
                     perm = rubik.perm_apply(move, curr)
@@ -66,7 +66,7 @@ def shortest_path(start, end):
                 return construct_path(overlap, Ldict, Rdict)
             else:
                 for move in rubik.quarter_twists:
-                    perm = rubik.perm_apply(move,curr)
+                    perm = rubik.perm_apply(move, curr)
                     if perm not in Rdict:
                         Rdict[perm] = (move, curr)
                         RQueue.append(perm)
@@ -76,9 +76,4 @@ def shortest_path(start, end):
     # end while
     return None
 
-"""
-if __name__ == "__main__":
-    start = (6, 7, 8, 20, 18, 19, 3, 4, 5, 16, 17, 15, 0, 1, 2, 14, 12, 13, 10, 11, 9, 21, 22, 23)
-    end = rubik.I
-    result = shortest_path(start, end)
-"""
+
