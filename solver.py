4750b92faf79579ecab2d8ec8ec5635b6963424e
zelano47 <andrew_zelano@my.uri.edu>
2020-03-04 10:53:57 -0500

    almost working?

--- solver.py
+++ solver.py
@@ -1,18 +1,23 @@
 import rubik
 
-def apply_all_perms(position):
-    perms = []
-    for move in rubik.quarter_twists_names:
-        perms.append(rubik.perm_apply(move, position))
-    return perms
-
-def construct_path(overlap, overlapMove, Ldict, Rdict):
+def construct_path(overlap, Ldict, Rdict):
     # prepend moves to left list
     # append moves to right list
-    Lpath = [overlapMove]
+    Lpath = []
     Rpath = []
+    Lcurr = overlap
+    Rcurr = overlap
     while True:
-        pass
+        if Ldict[Lcurr][0] == 's':
+            break
+        Lpath.insert(0, Ldict[Lcurr][0])
+        Lcurr = Ldict[Lcurr][1]
+    while True:
+        if Rdict[Rcurr][0] == 'e':
+            break
+        Rpath.append(Rdict[Rcurr][0])
+        Rcurr = Rdict[Rcurr][1]
+    
     return Lpath + Rpath
 
 
@@ -34,44 +39,46 @@ def shortest_path(start, end):
     RQueue = [end]
     Rcount = 1
     overlap = tuple()
-    overlapMove = ""
+    depth = 0
     while True:
+        if depth > 7:
+            print("Error: Invalid configuration")
+            return None
         newLCount = 0
         for i in range(Lcount):
-            if LQueue[i] in Rdict:
-                overlap = LQueue[i]
-                overlapMove = Rdict[overlap][0]
-                break
+            curr = LQueue.pop(0)
+            if curr in Rdict:
+                overlap = curr
+                return construct_path(overlap,Ldict, Rdict)
             else:
-                perms = apply_all_perms(LQueue[i])
-                for i in range(len(perms)):
-                    if perms[i] not in Ldict:
-                        Ldict[perms[i]] = (rubik.quarter_twists_names[i], perms[i])
-                        LQueue.append(perms[i])
+                for move in rubik.quarter_twists:
+                    perm = rubik.perm_apply(move, curr)
+                    if perm not in Ldict:
+                        Ldict[perm] = (move, curr)
+                        LQueue.append(perm)
                         newLCount+=1
         Lcount = newLCount
         newRCount = 0
         for i in range(Rcount):
-            if RQueue[i] in Ldict:
-                overlap = RQueue[i]
-                overlapMove = Rdict[overlap][0]
-                break
+            curr = RQueue.pop(0)
+            if curr in Ldict:
+                overlap = curr
+                return construct_path(overlap, Ldict, Rdict)
             else:
-                perms = apply_all_perms(RQueue[i])
-                for i in range(len(perms)):
-                    if perms[i] not in Rdict:
-                        Rdict[perms[i]] = (rubik.quarter_twists_names[i], perms[i])
-                        RQueue.append(perms[i])
+                for move in rubik.quarter_twists:
+                    perm = rubik.perm_apply(move,curr)
+                    if perm not in Rdict:
+                        Rdict[perm] = (move, curr)
+                        RQueue.append(perm)
                         newRCount+=1
-        Rcount = Lcount
-        
-    #construct path if an overlap is found
-    path = []
-    if overlap:
-        path = construct_path(overlap, overlapMove, Ldict, Rdict)
-    return path
+        Rcount = newRCount
+        depth+=1
+    # end while
+    return None
 
+"""
 if __name__ == "__main__":
     start = (6, 7, 8, 20, 18, 19, 3, 4, 5, 16, 17, 15, 0, 1, 2, 14, 12, 13, 10, 11, 9, 21, 22, 23)
     end = rubik.I
     result = shortest_path(start, end)
+"""
